749a9a9a6b76f2e3b3fd2127602586cfcd7a54f6
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-17 00:14:47 +0600

    08-Add ternary operator example in C++ with corresponding input and output files

---
+++ 008-ternary-operator/ternary-operator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+// #include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    /*
+        syntax :
+
+            condition ? True : False;
+    */
+
+    int x = 6;
+
+    (x % 2) == 0 ? cout << "Even" : cout << "Odd";
+
+    return 0;
+}
