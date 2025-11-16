0c846ce4b6c2f4b22ab501882f5bd2329e9cd266
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-17 02:12:19 +0600

    13-Add C++ program to read and output a string with spaces, including input and output files

---
+++ 013-string/b-with-space/string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+// #include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char s[101];
+    cin.getline(s, 101);
+
+    cout << s;
+
+    return 0;
+}
