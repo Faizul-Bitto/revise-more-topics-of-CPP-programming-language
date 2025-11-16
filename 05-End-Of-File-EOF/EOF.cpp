6bdd8530183b3403bbf170fbd8ac63de4d8c50fc
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-16 23:50:20 +0600

    05-Implement end-of-file input handling in C++ program with example input and output files

---
+++ 05-End-Of-File-EOF/EOF.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// #include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // take input until ends
+    int x;
+
+    while (cin >> x)
+    {
+        cout << x << endl;
+    }
+
+    return 0;
+}
