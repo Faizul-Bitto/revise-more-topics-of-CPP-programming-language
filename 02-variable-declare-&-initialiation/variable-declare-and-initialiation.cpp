5e042d77a4d400d48672db81914f709dbf45a85f
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-16 23:30:24 +0600

    02-Add variable declaration and initialization example in C++

---
+++ 02-variable-declare-&-initialiation/variable-declare-and-initialiation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+
+    char c = 'A';
+
+    double d = 14.56;
+
+    cout << x << "\n"
+         << c << endl
+         << d << endl;
+
+    cout << "My favorite number is " << x << endl;
+
+    return 0;
+}
