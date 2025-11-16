2af70a13857be5f97bc92de19d620124bbe9fa1c
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-17 01:48:46 +0600

    11-Add maximum value finder example in C++ with corresponding input and output files

---
+++ 011-max/max.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, f, g, h;
+    cin >> a >> b >> c >> d >> e >> f >> g >> h;
+
+    cout << max({a, b, c, d, e, f, g, h}) << endl;
+
+    return 0;
+}
