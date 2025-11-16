854f3794f95aa483090724004243358419875175
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-16 23:36:52 +0600

    03-Implement input handling and output display in C++ program

---
+++ 03-input-taking/input-taking.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// #include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    char c;
+    double d;
+
+    cin >> x >> c >> d;
+    cout << x << " " << c << " " << d << endl;
+
+    return 0;
+}
