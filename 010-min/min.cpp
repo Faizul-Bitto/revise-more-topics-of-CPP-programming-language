72e4e252d7141d217c4c64946ff0147f1bdfe4ce
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-17 01:47:03 +0600

    10-Add minimum value finder example in C++ with corresponding input and output files

---
+++ 010-min/min.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, f, g, h;
+    cin >> a >> b >> c >> d >> e >> f >> g >> h;
+
+    cout << min({a, b, c, d, e, f, g, h}) << endl;
+
+    return 0;
+}
