29db60b9d13c96de76924e71944e23cb9836f73f
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-17 15:02:14 +0600

    13-Add C++ program demonstrating string manipulation techniques, including input and output files

---
+++ 014-dynamic-variable/dynamic-variable.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x = 10; // normal variable
+
+    int *p = new int; //  dynamic variable
+
+    *p = 100;
+
+    cout << *p;
+
+    return 0;
+}
