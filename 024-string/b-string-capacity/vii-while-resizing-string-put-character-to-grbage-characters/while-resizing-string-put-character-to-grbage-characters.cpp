ee289d41445c4a261c31cbfd7148277665aabadd
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-27 01:40:41 +0600

    Add input and output files along with C++ program for resizing strings with character padding.

---
+++ 024-string/b-string-capacity/vii-while-resizing-string-put-character-to-grbage-characters/while-resizing-string-put-character-to-grbage-characters.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string S1 = "Hello World!";
+
+    S1.resize(15, 'x'); // Hello World! -> Hello World!xxx
+
+    cout << S1;
+
+    return 0;
+}
