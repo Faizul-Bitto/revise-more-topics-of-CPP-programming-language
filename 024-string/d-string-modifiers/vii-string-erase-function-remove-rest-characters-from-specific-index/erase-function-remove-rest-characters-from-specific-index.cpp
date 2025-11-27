3d21e911984a226419ff606bef5975a106d23db2
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-27 14:58:35 +0600

    Add C++ programs demonstrating the string erase function, including methods to remove characters from a specific index and to remove characters from a specific index to a specified number of characters, along with corresponding input and output files for each implementation.

---
+++ 024-string/d-string-modifiers/vii-string-erase-function-remove-rest-characters-from-specific-index/erase-function-remove-rest-characters-from-specific-index.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string S1 = "Hello World!";
+
+    S1.erase(5); // from 5 index, to rest
+
+    cout << S1;
+
+    return 0;
+}
---
+++ 024-string/d-string-modifiers/viii-string-erase-function-remove-from-specific-index-to-specific-number-of-characters/erase-function-remove-from-specific-index-to-specific-number-of-characters.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string S1 = "Hello World!";
+
+    S1.erase(5, 2);
+
+    cout << S1;
+
+    return 0;
+}
