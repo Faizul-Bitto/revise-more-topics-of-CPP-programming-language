5898540b9f630fc8d35fb48472307dc6477bb3c5
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-27 19:45:55 +0600

    Add C++ program for taking string input with spaces using getline, including source code, input, and output files.

---
+++ 024-string/f-take-string-input-with-space-with-getline-function/string-input-taking-with-getline-function.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int X;
+    cin >> X;
+    cin.ignore();
+
+    string S;
+    getline(cin, S);
+
+    cout << X << endl;
+    cout << S;
+
+    return 0;
+}
