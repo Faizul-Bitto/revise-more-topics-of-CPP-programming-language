cf398ad7a112fc2d22ea0f0cef0898fad0877757
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-11-30 10:53:14 +0600

    Add C++ program to sort an array of Student objects using a custom comparison function in descending order, along with input and output files for demonstration.

---
+++ 028-array-of-objects/e-sort-array-of-objects-with-custom-compare-function-descending-order/sort-array-of-objects-with-custom-compare-function-descendng-order.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
+
+bool cmp(Student left, Student right)
+{
+    if (left.marks > right.marks)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    // int a[10];
+    Student A[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i].name >> A[i].roll >> A[i].marks;
+    }
+
+    sort(A, (A + N), cmp);
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i].name << " " << A[i].roll << " " << A[i].marks << endl;
+    }
+
+    return 0;
+}
